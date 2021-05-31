96b63488f9335da50357765cd76667d020f158f0
Altricia Latimer <altricia.latimer@gmail.com>
2021-05-31 12:05:02 -0400

    working in app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 # Dependencies and Setup
 import numpy as np
 import datetime as dt
+import pandas as pd
 
 # Python SQL Toolkit and Object Relational Mapper
 import squlalchemy
@@ -16,7 +17,7 @@ from sqalchemy.pool import StaticPool
 # Database Setup
 #####################################
 # Reference
-engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={"check_same_thread": False}, poolclass=StaticPool, echo=True)
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # Reflect Existing Database Into a New Model
 Base = automap_base()
@@ -33,7 +34,7 @@ session = Session(engine)
 ###################################
 # Flask Setup
 ###################################
-app = Flask(_name_)
+app = Flask(__name__)
 
 ###################################
 # Flask Route
@@ -41,31 +42,14 @@ app = Flask(_name_)
 # Home Route
 @app.route("/")
 def welcome():
-       return"""<html>
-<h1>Hawaii Climate App (Flask API)</h1>
-<img src="https://i.ytimg.com/vi3ZMvhIO-d4/maxresdefault.jpg" alt="Hawaii Weather"/>
-<p>Precipation Analysis:</p>
-<ul>
-  <li><a href="/api/v1.0/precipitation">/api/v1.0/percipitation</a></li>
-</ul>
-<p>Station Anaylsis:</p>
-<ul>
-  <li.<a href="/api/v1.0/stations">/api/v1.0/stations</a><li>
-</ul>
-<p>Temperature Analysis:<p/>
-<ul>
-   <li><a href="/api/v1.0/tobs">/api/v1.0/tobs</a></li>
-</ul>
-<p>Start Day Analysis:</p>
-<ul>
-   <li><a href="/api/v1.0/2017-03-14">/api/v1.0/2017-03-14</a></li>
-<ul>
-<p>Start & End Day Analysis:</p>
-<ul>
-   <li><a href="/api/v1.0/2017-03-14/2017-03-28">/api/v1.0/2017-03-14/2017-03-28</a></li>
-</ul>
-</html>
-"""
+       return (
+           f"Welcome to the Hawaii Climate Analysis APT!<br/>"
+           f"Available Routes:<br/>"
+           f"/api/v1.0/precipitation<br/>"
+           f"/api/v1.0/stations<br/>"
+           f"/api/v1.0/tobs<br/>"
+           f"/api/v1.0/temps/start/end"
+       )
 
 # Precipitation Route
 @app.route("/api/v1.0/precipitation")
